1c57ec2bab5f8bf0f6eb576f43ee5419cef53e0b
isk221492 <106644670+isk221492@users.noreply.github.com>
2023-08-02 04:10:21 +0000

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
+	# /users へのURLアクセス時のアクション定義
+	# （アクション内容はControlleerで定義する）
+	resources :users
+		# 以下の挙動をまとめて定義できる
+		# /users -> users#index
+		# /users/new -> users#new
+		# /users/:id -> users#show
   root "hello#index"
 end
